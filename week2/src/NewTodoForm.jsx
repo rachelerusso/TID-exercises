96e90575071ba631dc58e211be9544c5f7e48dbc
raru <raru@itu.dk>
2026-09-15 14:30:58 +0200

    Add controlled NewTodoForm and connect it to TodoList

--- week2/src/NewTodoForm.jsx
+++ week2/src/NewTodoForm.jsx
@@ -1,10 +1,26 @@
-function NewTodoForm({ onAdd }) {
-    const [text, setText] = useState("");
+import { useState } from "react";
 
-function handleSubmit(e) {
+export default function NewTodoForm({ onAdd }) {
+  const [text, setText] = useState("");
+
+  function handleSubmit(e) {
     e.preventDefault();
     onAdd(text);
     setText("");
-}
-    return (<form onSubmit={handleSubmit}> </form>);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Write a new task..."
+        />
+        <button disabled={text.trim().length === 0} type="submit">
+          Add
+        </button>
+      </form>
+    </>
+  );
 }
--- week2/src/ToDoList.jsx
+++ week2/src/ToDoList.jsx
@@ -1,33 +1,38 @@
-//week2: Add useState for todos
+//week2: Add useState for todos + form
 import { useState } from "react";
+import NewTodoForm from "./NewTodoForm.jsx";
 
-  const SAMPLE_TASKS = ["Buy milk", "Call the landlord", "Book the dentist", "Water the plants"];
+/*const SAMPLE_TASKS = [
+  "Buy milk",
+  "Call the landlord",
+  "Book the dentist",
+  "Water the plants",
+];
 
-  function randomTask() {
-    return SAMPLE_TASKS[Math.floor(Math.random() * SAMPLE_TASKS.length)];
-  }
+function randomTask() {
+  return SAMPLE_TASKS[Math.floor(Math.random() * SAMPLE_TASKS.length)];
+}*/
 
-  function TodoItem({ text }) {
-    return <li>{text}</li>;         
-  }
+function TodoItem({ text }) {
+  return <li>{text}</li>;
+}
 
 export default function TodoList() {
+  const [todos, setTodos] = useState(["Buy milk"]); //initial state of the component
 
-  const [todos, setTodos] = useState(["Buy milk"]);   //initial state of the component
-
-  function handleAdd() {
-    setTodos([...todos, randomTask()]);   // Usi la funzione setter per aggiornare lo stato (creando un nuovo array, not a push)
+  function handleAdd(text) {
+    setTodos([...todos, text]); // Usi la funzione setter per aggiornare lo stato (creando un nuovo array, not a push)
   }
 
   return (
     <>
       <h1> My To Do List ({todos.length})</h1>
+      <NewTodoForm onAdd={handleAdd} />
       <ul>
-        {todos.map((text, index) => (              
+        {todos.map((text, index) => (
           <TodoItem key={index} text={text} />
         ))}
       </ul>
-      <button onClick={handleAdd}> Add </button>
     </>
   );
 }
